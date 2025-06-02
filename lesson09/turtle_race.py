787c15137124f9af7564c6bad917d93c0558a5a9
Vasyl Boliukh <vskesha@gmail.com>
2025-06-02 16:48:08 +0300

    draw start button

--- lesson09/turtle_race.py
+++ lesson09/turtle_race.py
@@ -18,9 +18,34 @@ def start_game(x, y):
     )
 
 # Функція для малювання кнопки "Почати гру"
+def draw_start_button():
+    w = 200
+    h = 50
+    t = turtle.Turtle()
+    t.speed(8)
+    t.penup()
+    t.goto(-w // 2, h)
+    t.pendown()
+    t.color("black", "green")
+    t.begin_fill()
+    for _ in range(2):
+        t.forward(w)
+        t.left(90)
+        t.forward(h)
+        t.left(90)
+    t.end_fill()
 
+    t.forward(w // 2)
+    t.color("white")
+    t.write(
+        "Почати гру",
+        font=("Arial", h // 2, "bold"),
+        align="center"
+    )
+    t.hideturtle()
 
 # Відслідковування натискання на кнопку
-start_game(0, 0)
+draw_start_button()
+screen.onscreenclick(start_game)
 
 turtle.done()
