a2cd99ccd95a087c6e3fa8625438d33ca2b56abd
Vasyl Boliukh <vskesha@gmail.com>
2025-06-02 19:28:31 +0300

    start_game

--- lesson09/turtle_race.py
+++ lesson09/turtle_race.py
@@ -4,12 +4,23 @@ import turtle
 
 # -----------------------------------------------------------
 
-# Функція для початку гри
+screen = turtle.Screen()
+screen.title("Черепаші перегони")
+screen.bgcolor("lightblue")
 
+# Функція для початку гри
+def start_game(x, y):
+    t = turtle.Turtle()
+    t.write(
+        "Гра розпочалася",
+        font=("Arial", 30, "bold"),
+        align="center"
+    )
 
 # Функція для малювання кнопки "Почати гру"
 
 
 # Відслідковування натискання на кнопку
+start_game(0, 0)
 
 turtle.done()
